4dd350f2b4a601a5c821478dc540d81d21c16d3b
DeepanshuManocha <dmanocha1509@gmail.com>
2020-04-18 19:08:33 +0530

    added collor picker

---
+++ Assets/Script/ApplyColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyColor : MonoBehaviour
+{
+    [SerializeField] private FlexibleColorPicker fcp;
+    [SerializeField] private Material[] material;
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i=0;i<material.Length;i++)
+        {
+            material[i].color = fcp.color;
+        }
+    }
+}

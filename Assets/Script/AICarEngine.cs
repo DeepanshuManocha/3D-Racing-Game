a2fccdc7829d4b35b94d51d830bc2a338da40656
DeepanshuManocha <dmanocha1509@gmail.com>
2020-04-28 11:12:51 +0530

    bugs fixed in path set for AI cars

---
+++ Assets/Script/AICarEngine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICarEngine : MonoBehaviour
+{
+    public Transform path;
+    private List<Transform> nodes = new List<Transform>();
+    private int currentNode = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform[] pathTransform = path.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < pathTransform.Length; i++)
+        {
+            if (pathTransform[i] != path.transform)
+                nodes.Add(pathTransform[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplySteer();
+    }
+
+    private void ApplySteer()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+        print(relativeVector);
+    }
+}

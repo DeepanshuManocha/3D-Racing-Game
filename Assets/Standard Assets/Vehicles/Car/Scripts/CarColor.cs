b9f4422671160b281961aab871796027300d30c0
DeepanshuManocha <dmanocha1509@gmail.com>
2020-04-28 08:01:27 +0530

    path set for AI cars

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarColor.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarColor.cs
@@ -2,18 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class CarColor : MonoBehaviour
+public class CarColor : MonoBehaviour
+{
+    public static Color color;
+    private static CarColor instance = null;
+    public static CarColor Instance
     {
-        public static Color color;
-        // Start is called before the first frame update
-        void Start()
-        {
-            DontDestroyOnLoad(gameObject);
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        get { return instance; }
     }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+// Update is called once per frame
+void Update()
+{
+
+}
+}
